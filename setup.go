337fe6e62c6a7a802c12c5a97df62839cac26926
Luke Moynihan <luke@bossinsights.com>
2019-11-10 17:45:50 -0500

    updated dependencies

--- setup.go
+++ setup.go
@@ -10,8 +10,8 @@ import (
 	"time"
 
 	"github.com/boltdb/bolt"
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 )
 
 func init() {
